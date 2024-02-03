669c16f02cf308e60584e1f896660b79dcb07da9
Burak <118461160+shutjs@users.noreply.github.com>
2024-02-03 21:02:03 +0300

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  Guild:{
+   ID: "",
+   botseskanal: ""
+  },
+  Prefix: ".",
+
+  Users: {
+    OWNERS: ["669513018777796638"] 
+  },
+
+  Handlers: {
+    MONGO: ""
+  },
+
+  Client: {
+    TOKEN: "", 
+    ID: "" 
+  }
+
+}
